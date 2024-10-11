950da508d3a9fc0dc86503ae2e055eb66b75ce29
Massive Devz <massivedevz@gmail.com>
2024-10-11 16:13:51 -0400

    update mobile styles

--- components/Home/portfolio/Portfolio.js
+++ components/Home/portfolio/Portfolio.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import pic1 from "../../../assets/images/work_image-1.jpeg";
+
 export default function Portfolio() {
   return (
     <section id="portfolio" className="portfolio">
@@ -19,10 +22,11 @@ export default function Portfolio() {
           <div className="row">
             <div className="col-4 img-container">
               <p>01</p>
-              <img
+              <Image src={pic1} />
+              {/* <img
                 src="https://images.unsplash.com/photo-1595475038784-bbe439ff41e6?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt=""
-              />
+              /> */}
               <div className="info">
                 <h5>Chef</h5>
                 <p>Sound · Meditation · Work</p>
--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,9 @@ export default function Home() {
       {/* <Header />
       <About />
       <Services /> */}
+
       <Portfolio />
+
       {/* <Team />
       <Brands />
       <Testimonials />
